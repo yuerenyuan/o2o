d4f09371322eef80259152d32dfcca5acc5ff091
yuerenyuan <945155148@qq.com>
2018-09-20 13:47:23 +0800

    logback日志功能

--- src/main/java/com/o2o/web/superadmin/AreaController.java
+++ src/main/java/com/o2o/web/superadmin/AreaController.java
@@ -2,6 +2,8 @@ package com.o2o.web.superadmin;
 
 import com.o2o.entity.Area;
 import com.o2o.service.AreaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +20,13 @@ import java.util.Map;
 @RestController
 @RequestMapping("/surperadmin")
 public class AreaController {
+    Logger logger=LoggerFactory.getLogger(AreaController.class);
     @Resource
     private AreaService areaService;
     @GetMapping("arealist")
     public Map<String,Object> areaList(){
+       logger.info("===start===");
+        Long startTime=System.currentTimeMillis();
         Map<String,Object> map=new HashMap<String,Object>();
         List<Area> list=new ArrayList<Area>();
         try{
@@ -33,6 +38,10 @@ public class AreaController {
             map.put("success",false);
             map.put("error",e.toString());
         }
+        logger.error("testError");
+        Long endTime=System.currentTimeMillis();
+        logger.debug("costTime:[{}ms]",endTime-startTime);
+        logger.info("===end===");
         return map;
     }
 }
